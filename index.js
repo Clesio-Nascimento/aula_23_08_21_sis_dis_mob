c3e234f3d17e4f57555d8064669cc761c9675ec9
Clesio <clesio.nascto@gmail.com>
2021-08-23 10:40:42 -0300

    Encerramos a parte de arrow function

--- index.js
+++ index.js
@@ -1,13 +1,108 @@
-let a
-let a
-var idade = 18
-console.log ("Oi, " + nome)
-if (idade >= 18){
-     let nome = "João"
-     console.log ("Parabéns " + nome + ". Você já pode dirigir")
+const triplo = (valor) => {
+     console.log("Agora estamos calculando o triplo de " + valor)
+     return 3 * valor
+}
+console.log (triplo(5))
+
+
+
+
+//const dobro = (valor) => valor * 2
+//console.log (dobro (10))
+
+//const hello = () => console.log("hello")
+//hello()
+
+
+
+//arrow function
+//const dobro = n => n * 2
+//console.log(dobro(2))
+
+
+
+/*function dobro (n){
+     return n * 2
+}
+*/
+
+
+
+/*const triplo = function (n = 5){
+     return n * 3
+}
+console.log (triplo(8))
+console.log (triplo())
+*/
+
+/*const triplo = function (n){
+     if (n !== undefined)
+          return 3 * n
+
+     return 3 *5 
+     
+}
+*/
+/*
+function eAgora (f1, f2){
+     return f1() * f1()
 }
 
- console.log ("Até mais, " + nome)
+console.log(eAgora (function () {return 2 + 3 }, function () {return 2 * 3}))
+
+*/
+
+/*
+const dobro = function (n) {
+     return n * 2
+}
+
+const res = dobro (5)
+console.log (res)
+*/
+
+
+
+/*
+
+
+function soma (a, b){
+     if (a === b){
+          return a + b
+     }
+}
+
+const resultado = soma (3, 3)
+const resultado = soma (2, 3)
+
+console.log(`resultado 1: ${resultado}. Resultado 2: ${resultado2}`)
+
+*/
+
+
+
+
+
+/*
+function hello(){
+      console.log('hello')
+ }
+hello ()
+
+function hello(nome){
+     console.log(`Hello, ${nome}`) //console.log("hello, " + nome)
+}
+hello ('José')
+*/
+
+
+
+
+
+
+
+
+
 
 
 // var linguagem = "Javascript"
